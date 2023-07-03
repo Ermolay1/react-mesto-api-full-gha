927f7b503fdc2d746462db5ad5ebd81b25470194
lesha <oookosmos.m@yandex.ru>
2023-07-03 11:44:28 +0300

    подключаю cors

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const helmet = require('helmet');
-//const cors = require('cors');
+const cors = require('cors');
 const { errors } = require('celebrate');
 const router = require('./routes/routes');
 const errorHandler = require('./middlewares/errorHandler');
@@ -14,7 +14,7 @@ const {
 const { PORT = 3000, bd = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
 const app = express();
-//app.use(cors());
+app.use(cors());
 
 app.get('/crash-test', () => {
   setTimeout(() => {
