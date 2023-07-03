22bce55d3dd6d97465f94634422a094cf7d715fa
lesha <oookosmos.m@yandex.ru>
2023-07-03 12:15:29 +0300

    Исправление ошибок тестов

--- backend/utils/constants.js
+++ backend/utils/constants.js
@@ -1,5 +1,6 @@
 const SECRET_KEY = 'dev-secret';
-const URL_REGEX = /(https?:\/\/)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&\/=]*)/;
+// eslint-disable-next-line no-useless-escape
+const URL_REGEX = /(https?:\/\/)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&\/=]*)/;
 
 module.exports = {
   SECRET_KEY,
